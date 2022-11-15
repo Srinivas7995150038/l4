c1f964a348efa6bac778b932cc0d69a1a641e61f
SRINIVAS BOKKALA <113031941+Srinivas7995150038@users.noreply.github.com>
2022-11-15 22:31:19 +0530

    updated file

---
+++ todo.js
@@ -0,0 +1,41 @@
+const todoList = () => {
+  all = [];
+  const add = (todoItem) => {
+    all.push(todoItem);
+  };
+  const markAsComplete = (index) => {
+    all[index].completed = true;
+  };
+
+  const overdue = () => {
+    return all.filter((todo) => todo.dueDate < today);
+  };
+
+  const dueToday = () => {
+    return all.filter((todo) => todo.dueDate == today);
+  };
+
+  const dueLater = () => {
+    return all.filter((todo) => todo.dueDate > today);
+  };
+
+  const toDisplayableList = (list) => {
+    return list
+      .map(
+        (todo) =>
+          `${todo.completed ? "[x]" : "[ ]"} ${todo.title} ${todo.dueDate == today ? "" : todo.dueDate
+          }`
+      )
+      .join("\n");
+  };
+
+  return {
+    all,
+    add,
+    markAsComplete,
+    overdue,
+    dueToday,
+    dueLater,
+    toDisplayableList,
+  };
+};
